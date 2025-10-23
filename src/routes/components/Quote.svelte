c4106eda9a9c9b4b3eab59048cc2472105102615
Michael Boateng <michaelboateng064@gmail.com>
2025-10-23 16:17:41 +0000

    Added a quote section

---
+++ src/routes/components/Quote.svelte
@@ -0,0 +1,17 @@
+<script>
+    import image from '$lib/assets/images/children-1144109_1280.jpg';
+</script>
+
+<section class="flex items-start justify-start container h-[300px] mx-auto mb-20  mt-50 px-30">
+    <div class=" h-full w-full bg-white overflow-hidden">
+        <img src={image} alt="" class="w-full h-full object-cover" />
+    </div>
+    <div class="bg-[#e07b1c] h-full text-white p-10">
+        <p class="pl-20 pt-16 italic">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, enim numquam cum voluptate incidunt nobis voluptates ex. Soluta, perspiciatis consequuntur.</p>
+
+        <div class="px-10 mt-10">
+            <h3 class="text-md font-bold">Michael Boateng</h3>
+            <p class="text-sm">SVA Student</p>
+        </div>
+    </div>
+</section>
