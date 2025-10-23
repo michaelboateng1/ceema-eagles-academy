f53b6807f0ecba1ed6be140e4946ca9e93e859d8
Michael Boateng <michaelboateng064@gmail.com>
2025-10-23 16:35:23 +0000

    Added StudentsWelBeing component

---
+++ src/routes/components/StudentsWellBeing.svelte
@@ -0,0 +1,20 @@
+<script>
+    import image from '$lib/assets/images/children-4768404_1280.jpg';
+</script>
+
+<section class="container mx-auto px-30 flex justify-center items-center gap-8 mb-20 mt-60">
+    <div>
+        <h2 class="uppercase font-bold mb-2 text-2xl">All students and stuff will feel safe & respected at sva</h2>
+    <p class="text-[#333] mb-5">The school environment (social, physical, emotional and behavioral) will be safe, welcoming, and conducive to learing.</p>
+    <ul class="text-[#333] space-y-3 text-sm list-disc list-insert px-10">
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident sunt ullam blanditiis! Nemo recusandae, et nisi quo corrupti officia! Excepturi!</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident sunt ullam blanditiis! Nemo recusandae, et nisi quo corrupti officia! Excepturi!</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident sunt ullam blanditiis! Nemo recusandae, et nisi quo corrupti officia! Excepturi!</li>
+    </ul>
+    </div>
+
+    <div class="h-auto w-full rounded-lg overflow-hidden">
+        <img src={image} alt="" class="w-full h-full object-cover" />
+    </div>
+    
+</section>
