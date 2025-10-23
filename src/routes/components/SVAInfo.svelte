cc662e649757676fc8780646798ac963f1f1291f
Michael Boateng <michaelboateng064@gmail.com>
2025-10-23 15:46:47 +0000

    Added small info to the home page

---
+++ src/routes/components/SVAInfo.svelte
@@ -0,0 +1,13 @@
+<script>
+    import SVAInfoCard from "./SVAInfoCard.svelte";
+</script>
+
+<section class="px-30 mt-60 mb-20">
+    <h2 class="capitalize text-2xl mb-10 font-bold">Lorem, ipsum dolor sit amet consectetur adipisicing.</h2>
+    <div class="grid grid-cols-2 container mx-auto gap-8">
+        <SVAInfoCard />
+        <SVAInfoCard />
+        <SVAInfoCard />
+        <SVAInfoCard />
+    </div>
+</section>
---
+++ src/routes/components/SVAInfoCard.svelte
@@ -0,0 +1,14 @@
+<script>
+    import icon1 from "$lib/assets/images/schoolLogo.jpg";
+</script>
+
+<section class="flex justify-center gap-3 items-start">
+    <div class="w-20 h-auto overflow-hidden rounded-full">
+        <img src={icon1} alt="" class="w-full h-full object-cover">
+    </div>
+    <div class="pt-3">
+        <h2 class="text-lg font-semibold mb-2">Our Facilities</h2>
+        <p class="text-sm text-[#333] w-[40ch]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum repudiandae esse quo molestiae voluptatem minus quisquam nesciunt unde odio rem.</p>
+        <a href="/about" class="capitalize text-[#e07b1c] block mt-2 text-sm">learn more</a>
+    </div>
+</section>
