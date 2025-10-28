a8548119e88fea02fe7b49376f837299e4e3bfeb
Michael Boateng <michaelboateng064@gmail.com>
2025-10-28 13:16:15 +0000

    Added a map to contact

---
+++ src/routes/components/Map.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import L from 'leaflet';
+    import 'leaflet/dist/leaflet.css';
+  
+    let mapContainer: HTMLDivElement;
+  
+    onMount(() => {
+      const map = L.map(mapContainer).setView([5.6037, -0.1870], 13); // Accra, Ghana
+  
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+      }).addTo(map);
+  
+      L.marker([5.6037, -0.1870]).addTo(map)
+        .bindPopup('Ceema Eagles Academy')
+        .openPopup();
+
+      L.marker([6.6037, -0.2870]).addTo(map)
+        .bindPopup('Hello from SmartTribe')
+        // .openPopup();
+    });
+  </script>
+
+  <div bind:this={mapContainer} id="map" class="h-[500px] w-[500px] z-0"></div>
+  
+
+  
+  
