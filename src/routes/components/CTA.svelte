f0a5c0f0736e682a689bf1244935db9a78e0530b
Michael Boateng <michaelboateng064@gmail.com>
2025-11-03 16:43:52 +0000

    Added CTA component

---
+++ src/routes/components/CTA.svelte
@@ -0,0 +1,6 @@
+<section class="mt-20 py-10">
+    <h2 class="capitalize font-bold mb-5 text-[#333] text-xl sm:text-2xl text-center">We prepare students for success in life</h2>
+    <div class="flex justify-center items-center">
+        <a href="/contact" class="py-2 px-10 font-semibold bg-[#ebeae5] rounded-4xl">Contact us</a>
+    </div>
+</section>
